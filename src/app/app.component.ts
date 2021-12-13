4449fb4eef1e80410c91e3d60002fedaa6351d1a
Adrian <egeaadrian14@gmail.com>
2021-12-13 13:06:04 +0100

    mas imagenes, recomendaciones terminadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filmotecangular';
+  tablon!: URL[];
+  edad = 0;
+  name = String;
+
+  urls = [{"name":"ebay","url":"http://ebay.com"},
+{"name":"amazon","url":"http://amazon.com"},
+{"name":"msn","url":"http://msn.com"},
+{"name":"yahoo","url":"http://yahoo.com"},
+{"name":"wikipedia","url":"http://wikipedia.org"}];
+
+  constructor() { }
+ngOnInit() {
+//this.tablon = ["https://www.py4u.net/discuss/1168541", "5", "8", "24", "32", "11", "55"];
+//this.fruits = ["pera", "manzana", "melón", "sandía", "mango"];
+}
+
   
 }
